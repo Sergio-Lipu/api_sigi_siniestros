89d4cddfb1371a73b092ffb8a368e21cec21054e
Sergio Ramirez <s.ramirez@lidcorp.mx>
2023-08-23 13:47:13 -0600

    agrega los metodos POST y PUT para los ajustadores

--- src/main/java/com/api/siniestro/controllers/AjustadorController.java
+++ src/main/java/com/api/siniestro/controllers/AjustadorController.java
@@ -1,11 +1,16 @@
 package com.api.siniestro.controllers;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.api.siniestro.entitys.Ajustador;
@@ -28,5 +33,29 @@ public class AjustadorController {
 	public List<Ajustador> obtenerAjustadores(){
 		return ajustadorService.obtenerAjustadores();
 		}
+	
+	@PostMapping
+    public ResponseEntity<Ajustador> crearAjustador(@RequestBody Ajustador ajustador) {
+        Ajustador nuevoAjustador = ajustadorService.crearAjustador(ajustador);
+        return ResponseEntity.ok(nuevoAjustador);
+    }
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Ajustador> actualizarAjustador(@PathVariable Long id,@RequestBody Ajustador ajustador){
+		Optional<Ajustador> ajustadorOpt = ajustadorService.obtenerAjustador(id);
+		if (!ajustadorOpt.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		Ajustador actualizarAjustador = ajustadorOpt.get();
+
+		actualizarAjustador.setNombre(ajustador.getNombre());
+		actualizarAjustador.setActivo(ajustador.getActivo());
+		actualizarAjustador.setIdUnidadNegocio(ajustador.getIdUnidadNegocio());
+		
+		Ajustador ajustadorGuardado = ajustadorService.actualizarAjustador(actualizarAjustador);
+		
+		return ResponseEntity.ok(ajustadorGuardado);
+	}
 
 }
--- src/main/java/com/api/siniestro/services/AjustadorService.java
+++ src/main/java/com/api/siniestro/services/AjustadorService.java
@@ -3,6 +3,8 @@ package com.api.siniestro.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.api.siniestro.entitys.Ajustador;
@@ -10,6 +12,7 @@ import com.api.siniestro.repositories.AjustadorRepository;
 
 @Service
 public class AjustadorService {
+	
 	@Autowired
 	 AjustadorRepository ajustadorRepository;
 	
@@ -19,4 +22,14 @@ public class AjustadorService {
 	 public Optional<Ajustador> obtenerAjustador(Long id) { 
 		 return ajustadorRepository.findById(id);
 		 }
+	 
+	 @Transactional
+	 public Ajustador crearAjustador(Ajustador ajustador){
+		 return ajustadorRepository.save(ajustador);
+	 }
+	 
+	 public Ajustador actualizarAjustador(Ajustador ajustador){
+		 return ajustadorRepository.save(ajustador);
+	 }
+	 
 }
